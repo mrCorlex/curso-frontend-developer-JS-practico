01c49a481286b1431bc7ea4963716a700aad9e3c
Arturo Gongora <arturogoni@hotmail.com>
2022-09-13 12:06:17 -0500

    shopping cart integrated, conflicts betwen menus solved

--- main.js
+++ main.js
@@ -2,14 +2,37 @@ const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const imgMenu = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu")
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart")
+const asideShoppingCart = document.querySelector(".product-detail");
 
+//menus closed const
+const isDesktopMenuClosed = desktopMenu.classList.contains("inactive");
+const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+const isAsideShoppingCartClosed = asideShoppingCart.classList.contains("inactive");
+
+/*event listeners*/ 
 navEmail.addEventListener("click", toggleDesktopMenu);
 imgMenu.addEventListener("click", toggleMobileMenu);
+shoppingCartIcon.addEventListener("click", toggleShoppingCart)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle("inactive");
+    if (isAsideShoppingCartClosed) {
+        asideShoppingCart.classList.add("inactive");
+    }    
 }
 
 function toggleMobileMenu() {
     mobileMenu.classList.toggle("inactive");
+    if (isAsideShoppingCartClosed) {
+        asideShoppingCart.classList.add("inactive");
+    } 
+}
+
+function toggleShoppingCart() {
+    asideShoppingCart.classList.toggle("inactive");
+    if (isDesktopMenuClosed) {
+        mobileMenu.classList.add("inactive");
+        desktopMenu.classList.add("inactive");
+    }    
 }
